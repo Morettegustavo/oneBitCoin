6bd0234e5a22bc5909e52e15e54e19979e84ef06
Gustavo Faria Morette <morettegustavo@gmail.com>
2024-02-01 14:33:08 -0300

    chore: building endpoint url

--- App.js
+++ App.js
@@ -5,6 +5,25 @@ import HistoryGraphic from './src/components/HistoryGraphic';
 import QuotationsList from './src/components/QuotationsList';
 import QuotationsItems from './src/components/QuotationsList/QuotationsItems';
 
+function addZero(number) {
+  if(number <= 9){
+    return "0" + number;
+  }
+  return number;
+}
+
+function url(qtdDays) {
+  const date = new Date();
+  const listLastDays = qtdDays;
+  const endDate = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDay())}`;
+  /* Ele seta a data com a data atual menos a quantidade de dias que foi passado como parametro  */
+  date.setDate(date.getDate() - listLastDays)
+  const startDate = `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDay())}`;
+
+
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`;
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
